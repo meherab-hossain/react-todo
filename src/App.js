54dfc40f01985b718a4ac5b8297ad4d3f17f102c
meherab-hossain <h.meherabhossain@gmail.com>
2022-02-02 20:24:00 +0600

    new project init

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
+      <h1>hello world</h1>
     </div>
   );
 }
