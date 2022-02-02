79b3c1a476bc4d5fafd448aefa3a79ccbdd4bd77
meherab-hossain <h.meherabhossain@gmail.com>
2022-02-02 19:02:45 +0600

    react basics
    1.worked on state props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className='text-3xl'>
-      hello Meherab
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
