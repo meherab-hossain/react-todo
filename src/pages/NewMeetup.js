612c80a833fb456e8933f272a847d02511a08f73
meherab-hossain <h.meherabhossain@gmail.com>
2022-02-04 21:47:47 +0600

    http post method used
    1.added http post request
    2.passed props value from child to parent
    3.useNavigate hooks to go back to other page

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,29 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-learning-79135-default-rtdb.firebaseio.com//meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
+    console.log(meetupData);
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
